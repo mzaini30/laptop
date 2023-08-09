b1825f17dcf385a51f2e4fa686b8c166630aa70d
mzaini30 <muhzaini30@gmail.com>
2023-08-10 00:00:53 +0800

    buku digital

---
+++ template/buku-digital/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Router from "svelte-spa-router";
+  // @ts-ignore
+  import routes from "virtual:generated-pages-svelte";
+  import "@unocss/reset/tailwind.css";
+  // import "warna-tailwind/warna.css";
+  import "./App.css";
+  import "uno.css";
+  import stat_aja from "stat-aja";
+
+  // stat_aja({ judul: "Judul Aplikasi", tipe: "Aplikasi Android" });
+</script>
+
+<a href="#/" class="sticky top-0 p-3 block border-b">Judul Aplikasi</a>
+<Router {routes} />
