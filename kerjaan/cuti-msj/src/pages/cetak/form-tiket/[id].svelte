f8ae3b8c495504208f5bacba5cf83edad3d0e4d9
mzaini30 <muhzaini30@gmail.com>
2023-08-14 15:49:48 +0800

    lagi ngerjakan tiket

---
+++ kerjaan/cuti-msj/src/pages/cetak/form-tiket/[id].svelte
@@ -0,0 +1,48 @@
+<script>
+  import logo from "../../../gambar/logo-msj.png";
+</script>
+
+<div>
+  <div class="kiri" />
+  <div class="kanan">
+    <div class="bagian-logo">
+      <img src={logo} alt="" />
+      <p>COAL MINING</p>
+      <p>Jl. Batubara No. 8 Samarinda - 75123</p>
+      <p>Tel. (0541) 733 277</p>
+      <p>Fax. (0541) 742 848</p>
+    </div>
+    <div class="kotak">
+      <p>Samarinda, 14 Agustus 2023</p>
+      <p>No. 491 /SPC/MSJ/ XII /LAMP/ 2022</p>
+    </div>
+    <div class="kotak">
+      <p>Kepada Yth.</p>
+      <p>
+        <span class="icon">Ø</span>
+        <span>Finance Manager </span>
+      </p>
+      <p>Bpk. Hendra Tjoa</p>
+    </div>
+    <div class="kotak">
+      <p>Perihal :</p>
+      <p><span class="icon">Ø </span></p>
+      <p>Permohonan Tiket Cuti</p>
+    </div>
+    <div class="kotak">
+      <p>Mengetahui,</p>
+      <p>AGUS WINARTO</p>
+      <p>(HRDS DEPT. HEAD)</p>
+    </div>
+  </div>
+</div>
+
+<style>
+  @font-face {
+    src: url("/wingding.ttf");
+    font-family: wingdings;
+  }
+  .icon {
+    font-family: wingdings;
+  }
+</style>
