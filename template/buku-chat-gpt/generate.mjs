036139026606abee6bab5b7cd4ce9c49f2a6d3df
mzaini30 <muhzaini30@gmail.com>
2023-08-17 20:10:16 +0800

    template buku chat gpt selesai

---
+++ template/buku-chat-gpt/generate.mjs
@@ -0,0 +1,34 @@
+import { writeFileSync } from "fs";
+
+let list = `
+1. Lomba Tari Tradisional Daerah
+10. Lomba Cipta Lagu Kemerdekaan
+100. Lomba Panahan dengan Target Bendera
+`;
+
+// Membersihkan teks dan memisahkan setiap baris menjadi array
+let lines = list.trim().split("\n");
+
+// Mengolah setiap baris untuk membuat objek dalam array
+let olah_list = lines.map((line) => {
+  let match = line.match(/^(\d+)\.\s(.+)/); // Mencocokkan nomor dan judul
+  if (match) {
+    let nomor = parseInt(match[1]);
+    let judul = match[2].trim();
+    let slug = judul.toLowerCase().replace(/\s+/g, "-"); // Membuat slug
+
+    return { judul, slug };
+  }
+});
+
+// console.log(olah_list);
+for (let x of olah_list) {
+  writeFileSync(
+    `src/pages/uraian/${x.slug}.md`,
+    `---
+judul: ${x.judul}
+---
+
+`,
+  );
+}
--- template/buku-chat-gpt/vite.config.js
+++ template/buku-chat-gpt/vite.config.js
@@ -3,15 +3,27 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import legacy from "@vitejs/plugin-legacy";
 import pages from "vite-plugin-pages-svelte";
 import uno_css from "unocss/vite";
-import transformerVariantGroup from '@unocss/transformer-variant-group'
+import { presetTypography, presetUno } from "unocss";
+import transformerVariantGroup from "@unocss/transformer-variant-group";
+import { mdsvex } from "mdsvex";
 
-
-let plugins = [svelte(), pages(), uno_css({
-  // ...
-  transformers: [
-    transformerVariantGroup(),
-  ],
-})];
+let plugins = [
+  svelte({
+    extensions: [".svelte", ".svx", ".md"],
+    preprocess: mdsvex({
+      extensions: [".md"],
+      layout: "./src/layout.svelte",
+    }),
+  }),
+  pages({
+    extensions: ["svelte", "md"],
+  }),
+  uno_css({
+    // ...
+    transformers: [transformerVariantGroup()],
+    presets: [presetUno(), presetTypography()],
+  }),
+];
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
