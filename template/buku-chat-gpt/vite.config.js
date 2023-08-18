b13f2094c5890a21e586dc7531490e0667bbe4b1
mzaini30 <muhzaini30@gmail.com>
2023-08-18 13:53:42 +0800

    simpan dulu

--- template/buku-chat-gpt/vite.config.js
+++ template/buku-chat-gpt/vite.config.js
@@ -6,6 +6,23 @@ import uno_css from "unocss/vite";
 import { presetTypography, presetUno } from "unocss";
 import transformerVariantGroup from "@unocss/transformer-variant-group";
 import { mdsvex } from "mdsvex";
+// import viteMdx from "vite-plugin-mdx";
+
+let pengaturan_lainnya = {
+  build: {
+    rollupOptions: {
+      // ... (konfigurasi rollup lainnya)
+      output: {
+        manualChunks(id) {
+          // Pisahkan file .md menjadi chunk terpisah
+          if (id.endsWith(".md")) {
+            return "md-chunks/" + id.slice(id.lastIndexOf("/") + 1, -3);
+          }
+        },
+      },
+    },
+  },
+};
 
 let plugins = [
   svelte({
@@ -30,10 +47,12 @@ export default defineConfig(({ mode }) => {
   if (mode == "android") {
     return {
       plugins: [legacy(), ...plugins],
+      ...pengaturan_lainnya,
     };
   } else {
     return {
       plugins: [...plugins],
+      ...pengaturan_lainnya,
     };
   }
 });
