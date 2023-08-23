ac97284997c9392b5e8ad360fb1efb46f7f4d495
mzaini30 <muhzaini30@gmail.com>
2023-08-23 21:04:08 +0800

    masih ngolah tampilan form tiket

--- kerjaan/cuti-msj/src/pages/cetak/form-tiket/[id].svelte
+++ kerjaan/cuti-msj/src/pages/cetak/form-tiket/[id].svelte
@@ -1,10 +1,13 @@
 <script>
-  import logo from "../../../gambar/logo-msj.png";
+  import BagianLogo from "./bagian-logo.svelte";
 </script>
 
 <div class="grid grid-cols-115">
   <div class="kiri col-span-65">
-    <p>PT. MAHAKAM SUMBER JAYA</p>
+    <div class="invisible">
+      <BagianLogo />
+    </div>
+    <p class="font-bold mb-3">PT. MAHAKAM SUMBER JAYA</p>
     <p>Dengan hormat,</p>
     <p>Sehubungan dengan pelaksanaan cuti saya pada</p>
     <p>tanggal 3-Jan-23 s/d 7-Jan-23</p>
@@ -13,8 +16,10 @@
     <p><span class="icon">Ø</span> Point of hire : Yogyakarta</p>
     <p><span class="icon">Ø</span> Date of hire : 01 November 2000</p>
     <p><span class="icon">Ø</span> Adapun tiket akan saya minta dalam bentuk</p>
-    <p>uang tunai</p>
-    <table>
+    <p class="font-bold underline">uang tunai</p>
+    <table
+      class="[&_td]:(border border-black px-1) [&_th]:(border border-black px-1)"
+    >
       <thead>
         <tr>
           <th>No</th>
@@ -25,25 +30,25 @@
       </thead>
       <tbody>
         <tr>
-          <td>1.</td>
+          <td class="text-center">1.</td>
           <td>Siswanto</td>
           <td>Karyawan ybs.</td>
           <td>Rp 1.720.000</td>
         </tr>
         <tr>
-          <td>2.</td>
+          <td class="text-center">2.</td>
           <td>Yulia</td>
           <td>Istri</td>
           <td>Rp 1.000.000</td>
         </tr>
         <tr>
-          <td>3.</td>
+          <td class="text-center">3.</td>
           <td>Hendra</td>
           <td>Anak ke 1</td>
           <td>Rp 1.000.000</td>
         </tr>
         <tr>
-          <td>4.</td>
+          <td class="text-center">4.</td>
           <td>Rico</td>
           <td>Anak ke 2</td>
           <td>Rp 1.000.000</td>
@@ -58,39 +63,45 @@
     </table>
     <p>Demikian disampaikan, atas perhatiannya saya</p>
     <p>ucapkan terima kasih.</p>
-    <p>Hormat saya,</p>
-    <p>Siswanto</p>
+    <p class="py-10 pb-16">Hormat saya,</p>
+    <p class="font-bold underline">Siswanto</p>
     <p>Karyawan ybs.</p>
   </div>
-  <div class="kanan col-span-50">
-    <div class="bagian-logo">
+  <div
+    class="kanan col-span-50 last:[&_.kotak]:border-b [&_.kotak]:(border border-black border-b-0 p-1)"
+  >
+    <BagianLogo />
+    <!-- <div class="bagian-logo">
       <img src={logo} alt="" />
-      <p>COAL MINING</p>
-      <p>Jl. Batubara No. 8 Samarinda - 75123</p>
-      <p>Tel. (0541) 733 277</p>
-      <p>Fax. (0541) 742 848</p>
-    </div>
-    <div class="kotak">
+      <p class="font-bold text-sm">COAL MINING</p>
+      <p class="text-sm">Jl. Batubara No. 8 Samarinda - 75123</p>
+      <p class="text-sm">Tel. (0541) 733 277</p>
+      <p class="text-sm">Fax. (0541) 742 848</p>
+    </div> -->
+    <div class="kotak mt-15 [&>*]:pb-3">
       <p>Samarinda, 14 Agustus 2023</p>
       <p>No. 491 /SPC/MSJ/ XII /LAMP/ 2022</p>
     </div>
-    <div class="kotak">
+    <div class="kotak !py-3">
       <p>Kepada Yth.</p>
-      <p>
-        <span class="icon">Ø</span>
-        <span>Finance Manager </span>
-      </p>
-      <p>Bpk. Hendra Tjoa</p>
+      <div class="py-3">
+        <p class="pl-5">
+          <span class="icon">Ø</span>
+          <span>Finance Manager </span>
+        </p>
+        <p class="pl-10">Bpk. Hendra Tjoa</p>
+      </div>
     </div>
-    <div class="kotak">
+    <div class="kotak !py-3">
       <p>Perihal :</p>
-      <p><span class="icon">Ø </span></p>
-      <p>Permohonan Tiket Cuti</p>
+      <p class="py-3 pl-5"><span class="icon">Ø</span> Permohonan Tiket Cuti</p>
     </div>
-    <div class="kotak">
-      <p>Mengetahui,</p>
-      <p>AGUS WINARTO</p>
-      <p>(HRDS DEPT. HEAD)</p>
+    <div class="kotak !py-3">
+      <p class="pt-7 pb-20">Mengetahui,</p>
+      <div class="text-center">
+        <p class="font-bold underline">AGUS WINARTO</p>
+        <p>(HRDS DEPT. HEAD)</p>
+      </div>
     </div>
   </div>
 </div>
---
+++ kerjaan/cuti-msj/src/pages/cetak/form-tiket/bagian-logo.svelte
@@ -0,0 +1,11 @@
+<script>
+  import logo from "../../../gambar/logo-msj.png";
+</script>
+
+<div class="bagian-logo mb-5">
+  <img src={logo} alt="" />
+  <p class="font-bold text-sm">COAL MINING</p>
+  <p class="text-sm">Jl. Batubara No. 8 Samarinda - 75123</p>
+  <p class="text-sm">Tel. (0541) 733 277</p>
+  <p class="text-sm">Fax. (0541) 742 848</p>
+</div>
