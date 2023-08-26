63f1b493b64033e4d3d92aa8c520cb9884df09a8
mzaini30 <muhzaini30@gmail.com>
2023-08-27 07:29:41 +0800

    simpan dulu

---
+++ aplikasi-android/ornamen-kaligrafi/src/pages/index.svelte
@@ -0,0 +1,6 @@
+<div class="p-3">
+  <a
+    class="bg-green-500 p-2 text-white text-center w-full block rounded"
+    href="https://chat.whatsapp.com/DfmZzWvm5Kr6JLBdPW1Jhs">Join Komunitas</a
+  >
+</div>
