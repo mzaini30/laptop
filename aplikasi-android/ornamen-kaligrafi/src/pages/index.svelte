e8ff6dee4ee5083b9e20376308dd524c9effeb94
mzaini30 <muhzaini30@gmail.com>
2023-09-04 23:29:52 +0800

    tinggal build

--- aplikasi-android/ornamen-kaligrafi/src/pages/index.svelte
+++ aplikasi-android/ornamen-kaligrafi/src/pages/index.svelte
@@ -1,10 +1,3 @@
-<div class="p-3">
-  <a
-    class="bg-green-500 p-2 text-white text-center w-full block rounded"
-    href="https://chat.whatsapp.com/DfmZzWvm5Kr6JLBdPW1Jhs">Join Komunitas</a
-  >
-</div>
-
 <div class="grid mt-3 grid-cols-2">
   {#each Array(23) as x, n}
     <a href="#/{n + 1}">
@@ -15,4 +8,10 @@
       />
     </a>
   {/each}
+</div>
+<div class="p-3">
+  <a
+    class="bg-green-500 p-2 text-white text-center w-full block rounded"
+    href="https://support-zen.vercel.app/">Support Me</a
+  >
 </div>
