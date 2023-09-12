593dd84fd5c59c19a90cfe82b792ea1931223c5a
mzaini30 <muhzaini30@gmail.com>
2023-09-12 12:19:34 +0800

    simpan dulu

---
+++ aplikasi-android/semangat-ngaji/src/pages/index.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { push } from "svelte-spa-router";
+
+  let halaman_terakhir = 1;
+  let semua_data = []; // terbaru = paling atas (tanggal, banyaknya)
+
+  if (localStorage.halaman_terakhir) {
+    halaman_terakhir = JSON.parse(localStorage.halaman_terakhir);
+  }
+  if (localStorage.semua_data) {
+    semua_data = JSON.parse(localStorage.semua_data);
+  }
+</script>
+
+<div class="p-3">
+  <div class="card card-compact w-full bg-base-100 shadow-xl">
+    <div class="card-body">
+      <h2 class="card-title">Halaman Terakhir</h2>
+      <p>{halaman_terakhir}</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary" on:click={() => push("/tambah-baru")}
+          >Update</button
+        >
+      </div>
+    </div>
+  </div>
+</div>
--- aplikasi-android/semangat-ngaji/tailwind.config.js
+++ aplikasi-android/semangat-ngaji/tailwind.config.js
@@ -5,4 +5,29 @@ export default {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#7bc6ce",
+
+          secondary: "#289bcc",
+
+          accent: "#1f3de2",
+
+          neutral: "#2d2136",
+
+          "base-100": "#f4f5f6",
+
+          info: "#0f87f0",
+
+          success: "#138141",
+
+          warning: "#f9d553",
+
+          error: "#f14176",
+        },
+      },
+    ],
+  },
 };
