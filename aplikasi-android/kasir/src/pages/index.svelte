d45fc2aa555722852c6260379a396e749e319d77
mzaini30 <muhzaini30@gmail.com>
2023-09-16 16:04:35 +0800

    simpan dulu

--- aplikasi-android/kasir/src/pages/index.svelte
+++ aplikasi-android/kasir/src/pages/index.svelte
@@ -1,9 +1,55 @@
 <script>
   let hasil_pencarian = [];
+  let teks_cari = "";
+
+  let semua_data = [];
+  if (localStorage.semua_data) {
+    semua_data = JSON.parse(localStorage.semua_data);
+  }
+
+  let list_pembelian = []; // id_transaksi, nama_barang, harga_beli, harga_jual, banyak (1)
+  if (localStorage.list_pembelian) {
+    list_pembelian = JSON.parse(localStorage.list_pembelian);
+  }
+
+  function tambahkan_ke_list_pembelian(nama_barang, harga_beli, harga_jual) {
+    let sudah_ada = false;
+    for (let x of list_pembelian) {
+      if (x.nama_barang == nama_barang) {
+        sudah_ada = true;
+      }
+    }
+    if (sudah_ada == false) {
+      list_pembelian = [
+        ...list_pembelian,
+        {
+          id_transaksi: crypto.randomUUID(),
+          nama_barang,
+          harga_beli,
+          harga_jual,
+          banyak: 1,
+        },
+      ];
+    } else {
+      let dapatkan_banyak = [...list_pembelian].filter(
+        (x) => x.nama_barang == nama_barang,
+      )[0].banyak;
+      let data_baru = {
+        id_transaksi: crypto.randomUUID(),
+        nama_barang,
+        harga_beli,
+        harga_jual,
+        banyak: +dapatkan_banyak + 1,
+      };
+      // ini belum selesai
+    }
+    localStorage.list_pembelian = JSON.stringify(list_pembelian);
+  }
 </script>
 
 <input
-  type="text"
+  type="search"
+  bind:value={teks_cari}
   placeholder="Nama barang"
   class="input input-bordered block w-full"
 />
@@ -14,31 +60,46 @@
         <span class="label-text">Hasil Pencarian</span>
       </label>
       <ul class="menu bg-base-200 w-full rounded-box">
-        <li>
-          <a class="" href="#/">
-            <span>a</span>
-            <span class="badge">5000</span>
-          </a>
-        </li>
+        {#each semua_data.filter((y) => y.nama_barang
+            .toLowerCase()
+            .includes(teks_cari.toLowerCase())) as x}
+          <li>
+            <a
+              class=""
+              href="#/"
+              on:click|preventDefault={() =>
+                tambahkan_ke_list_pembelian(
+                  x.nama_barang,
+                  x.harga_beli,
+                  x.harga_jual,
+                )}
+            >
+              <span>{x.nama_barang}</span>
+              <span class="badge">{(+x.harga_jual).toLocaleString()} ➡️</span>
+            </a>
+          </li>
+        {/each}
       </ul>
     </div>
   </div>
   <div>
     <p class="[&>*]:text-xl btn mt-2 flex justify-between">
       <span>Total</span>
-      <span>3000000</span>
+      <span>3,000,000</span>
     </p>
     <div class="form-control w-full">
       <label class="label">
         <span class="label-text">List Pembelian</span>
       </label>
       <ul class="menu bg-base-200 w-full rounded-box">
-        <li>
-          <a class="" href="#/">
-            <span>a</span>
-            <span class="badge">5000</span>
-          </a>
-        </li>
+        {#each list_pembelian as x}
+          <li>
+            <a class="" href="#/edit-keranjang/1">
+              <span>{x.nama_barang} ({x.banyak})</span>
+              <span class="badge">{(+x.harga_jual).toLocaleString()}</span>
+            </a>
+          </li>
+        {/each}
       </ul>
     </div>
   </div>
