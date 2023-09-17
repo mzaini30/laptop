0b95dc1dc983c28ae0e4586dd34d380a9283dc4c
mzaini30 <muhzaini30@gmail.com>
2023-09-17 20:04:28 +0800

    simpan

--- aplikasi-android/doa/tailwind.config.js
+++ aplikasi-android/doa/tailwind.config.js
@@ -5,4 +5,29 @@ export default {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#8f68d6",
+
+          secondary: "#dea6fc",
+
+          accent: "#fff2b5",
+
+          neutral: "#1f182b",
+
+          "base-100": "#e8e9ed",
+
+          info: "#3b7ae8",
+
+          success: "#66ea99",
+
+          warning: "#e8c517",
+
+          error: "#f7514b",
+        },
+      },
+    ],
+  },
 };
