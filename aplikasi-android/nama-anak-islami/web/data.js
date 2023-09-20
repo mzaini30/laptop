7ab32f79c06f7d79db2b5fbed85bc671402fd395
mzaini30 <muhzaini30@gmail.com>
2023-09-21 07:50:22 +0800

    simpan

---
+++ aplikasi-android/nama-anak-islami/web/data.js
@@ -0,0 +1,45 @@
+export default function () {
+  let data = [
+    {
+      nama: "Muhammad",
+      artinya: "terpuji",
+    },
+    {
+      nama: "Aisha",
+      artinya: "hidup",
+    },
+    {
+      nama: "Ali",
+      artinya: "mulia",
+    },
+    {
+      nama: "Fatimah",
+      artinya: "berkilauan",
+    },
+    {
+      nama: "Yusuf",
+      artinya: "penyayang",
+    },
+    {
+      nama: "Amina",
+      artinya: "aman",
+    },
+    {
+      nama: "Ibrahim",
+      artinya: "teman yang setia",
+    },
+    {
+      nama: "Khadijah",
+      artinya: "dalam",
+    },
+    {
+      nama: "Hassan",
+      artinya: "baik",
+    },
+    {
+      nama: "Maryam",
+      artinya: "putri",
+    },
+  ];
+  return data;
+}
