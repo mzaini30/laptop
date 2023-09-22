fcb4f05a25e3be5175d353e18e45fc31ecd44f3d
mzaini30 <muhzaini30@gmail.com>
2023-09-22 15:18:42 +0800

    udahan lah

--- aplikasi-android/nama-anak-islami/web/data.js
+++ aplikasi-android/nama-anak-islami/web/data.js
@@ -1,250 +1,192 @@
-export default function () {
-  let data = [
-    {
-      nama: "Muhammad",
-      artinya: "terpuji",
-    },
-    {
-      nama: "Aisha",
-      artinya: "hidup",
-    },
-    {
-      nama: "Ali",
-      artinya: "mulia",
-    },
-    {
-      nama: "Fatimah",
-      artinya: "berkilauan",
-    },
-    {
-      nama: "Yusuf",
-      artinya: "penyayang",
-    },
-    {
-      nama: "Amina",
-      artinya: "aman",
-    },
-    {
-      nama: "Ibrahim",
-      artinya: "teman yang setia",
-    },
-    {
-      nama: "Khadijah",
-      artinya: "dalam",
-    },
-    {
-      nama: "Hassan",
-      artinya: "baik",
-    },
-    {
-      nama: "Maryam",
-      artinya: "putri",
-    },
-  ];
-  let data2 = [
-    {
-      nama: "Ahmad",
-      artinya: "pemimpin",
-    },
-    {
-      nama: "Laila",
-      artinya: "malam",
-    },
-    {
-      nama: "Husain",
-      artinya: "baik dan cantik",
-    },
-    {
-      nama: "Aisyah",
-      artinya: "hidup",
-    },
-    {
-      nama: "Rizki",
-      artinya: "berkah",
-    },
-    {
-      nama: "Yasin",
-      artinya: "kaya",
-    },
-    {
-      nama: "Siti",
-      artinya: "wanita",
-    },
-    {
-      nama: "Faisal",
-      artinya: "pengambil keputusan",
-    },
-    {
-      nama: "Aziz",
-      artinya: "mulia",
-    },
-    {
-      nama: "Farida",
-      artinya: "unik",
-    },
-    {
-      nama: "Rahmat",
-      artinya: "rahmat",
-    },
-    {
-      nama: "Sofia",
-      artinya: "bijak",
-    },
-    {
-      nama: "Nabil",
-      artinya: "pintar",
-    },
-    {
-      nama: "Zainab",
-      artinya: "bunga",
-    },
-    {
-      nama: "Ismail",
-      artinya: "Allah mendengar",
-    },
-    {
-      nama: "Nadia",
-      artinya: "tenang",
-    },
-    {
-      nama: "Adnan",
-      artinya: "tinggi",
-    },
-    {
-      nama: "Rania",
-      artinya: "ratu",
-    },
-    {
-      nama: "Malik",
-      artinya: "raja",
-    },
-    {
-      nama: "Saida",
-      artinya: "beruntung",
-    },
-    {
-      nama: "Yahya",
-      artinya: "hidup",
-    },
-    {
-      nama: "Sara",
-      artinya: "bunga",
-    },
-    {
-      nama: "Ismahan",
-      artinya: "cantik",
-    },
-    {
-      nama: "Nashit",
-      artinya: "pemberani",
-    },
-    {
-      nama: "Alya",
-      artinya: "mulia",
-    },
-  ];
-  let data3 = [
-    {
-      nama: "Rafi",
-      artinya: "tinggi",
-    },
-    {
-      nama: "Zara",
-      artinya: "murni",
-    },
-    {
-      nama: "Rayhan",
-      artinya: "baik",
-    },
-    {
-      nama: "Khadijah",
-      artinya: "berkilauan",
-    },
-    {
-      nama: "Ismail",
-      artinya: "Allah mendengar",
-    },
-    {
-      nama: "Dina",
-      artinya: "agung",
-    },
-    {
-      nama: "Sulaiman",
-      artinya: "pemurah",
-    },
-    {
-      nama: "Najwa",
-      artinya: "sembunyi",
-    },
-    {
-      nama: "Yahya",
-      artinya: "hidup",
-    },
-    {
-      nama: "Aminah",
-      artinya: "aman",
-    },
-    {
-      nama: "Imran",
-      artinya: "panjang umur",
-    },
-    {
-      nama: "Siti",
-      artinya: "wanita",
-    },
-    {
-      nama: "Amirah",
-      artinya: "pemimpin",
-    },
-    {
-      nama: "Ismahan",
-      artinya: "cantik",
-    },
-    {
-      nama: "Hakim",
-      artinya: "bijaksana",
-    },
-    {
-      nama: "Nadia",
-      artinya: "tenang",
-    },
-    {
-      nama: "Yusuf",
-      artinya: "penyayang",
-    },
-    {
-      nama: "Amina",
-      artinya: "aman",
-    },
-    {
-      nama: "Ibrahim",
-      artinya: "teman yang setia",
-    },
-    {
-      nama: "Zainab",
-      artinya: "bunga",
-    },
-    {
-      nama: "Ahmad",
-      artinya: "pemimpin",
-    },
-    {
-      nama: "Hafsa",
-      artinya: "pelindung",
-    },
-    {
-      nama: "Faisal",
-      artinya: "pengambil keputusan",
-    },
-    {
-      nama: "Farida",
-      artinya: "unik",
-    },
-    {
-      nama: "Malik",
-      artinya: "raja",
-    },
-  ];
+"use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = _default;
+function _default() {
+  var data = [{
+    nama: "Muhammad",
+    artinya: "terpuji"
+  }, {
+    nama: "Aisha",
+    artinya: "hidup"
+  }, {
+    nama: "Ali",
+    artinya: "mulia"
+  }, {
+    nama: "Fatimah",
+    artinya: "berkilauan"
+  }, {
+    nama: "Yusuf",
+    artinya: "penyayang"
+  }, {
+    nama: "Amina",
+    artinya: "aman"
+  }, {
+    nama: "Ibrahim",
+    artinya: "teman yang setia"
+  }, {
+    nama: "Khadijah",
+    artinya: "dalam"
+  }, {
+    nama: "Hassan",
+    artinya: "baik"
+  }, {
+    nama: "Maryam",
+    artinya: "putri"
+  }];
+  var data2 = [{
+    nama: "Ahmad",
+    artinya: "pemimpin"
+  }, {
+    nama: "Laila",
+    artinya: "malam"
+  }, {
+    nama: "Husain",
+    artinya: "baik dan cantik"
+  }, {
+    nama: "Aisyah",
+    artinya: "hidup"
+  }, {
+    nama: "Rizki",
+    artinya: "berkah"
+  }, {
+    nama: "Yasin",
+    artinya: "kaya"
+  }, {
+    nama: "Siti",
+    artinya: "wanita"
+  }, {
+    nama: "Faisal",
+    artinya: "pengambil keputusan"
+  }, {
+    nama: "Aziz",
+    artinya: "mulia"
+  }, {
+    nama: "Farida",
+    artinya: "unik"
+  }, {
+    nama: "Rahmat",
+    artinya: "rahmat"
+  }, {
+    nama: "Sofia",
+    artinya: "bijak"
+  }, {
+    nama: "Nabil",
+    artinya: "pintar"
+  }, {
+    nama: "Zainab",
+    artinya: "bunga"
+  }, {
+    nama: "Ismail",
+    artinya: "Allah mendengar"
+  }, {
+    nama: "Nadia",
+    artinya: "tenang"
+  }, {
+    nama: "Adnan",
+    artinya: "tinggi"
+  }, {
+    nama: "Rania",
+    artinya: "ratu"
+  }, {
+    nama: "Malik",
+    artinya: "raja"
+  }, {
+    nama: "Saida",
+    artinya: "beruntung"
+  }, {
+    nama: "Yahya",
+    artinya: "hidup"
+  }, {
+    nama: "Sara",
+    artinya: "bunga"
+  }, {
+    nama: "Ismahan",
+    artinya: "cantik"
+  }, {
+    nama: "Nashit",
+    artinya: "pemberani"
+  }, {
+    nama: "Alya",
+    artinya: "mulia"
+  }];
+  var data3 = [{
+    nama: "Rafi",
+    artinya: "tinggi"
+  }, {
+    nama: "Zara",
+    artinya: "murni"
+  }, {
+    nama: "Rayhan",
+    artinya: "baik"
+  }, {
+    nama: "Khadijah",
+    artinya: "berkilauan"
+  }, {
+    nama: "Ismail",
+    artinya: "Allah mendengar"
+  }, {
+    nama: "Dina",
+    artinya: "agung"
+  }, {
+    nama: "Sulaiman",
+    artinya: "pemurah"
+  }, {
+    nama: "Najwa",
+    artinya: "sembunyi"
+  }, {
+    nama: "Yahya",
+    artinya: "hidup"
+  }, {
+    nama: "Aminah",
+    artinya: "aman"
+  }, {
+    nama: "Imran",
+    artinya: "panjang umur"
+  }, {
+    nama: "Siti",
+    artinya: "wanita"
+  }, {
+    nama: "Amirah",
+    artinya: "pemimpin"
+  }, {
+    nama: "Ismahan",
+    artinya: "cantik"
+  }, {
+    nama: "Hakim",
+    artinya: "bijaksana"
+  }, {
+    nama: "Nadia",
+    artinya: "tenang"
+  }, {
+    nama: "Yusuf",
+    artinya: "penyayang"
+  }, {
+    nama: "Amina",
+    artinya: "aman"
+  }, {
+    nama: "Ibrahim",
+    artinya: "teman yang setia"
+  }, {
+    nama: "Zainab",
+    artinya: "bunga"
+  }, {
+    nama: "Ahmad",
+    artinya: "pemimpin"
+  }, {
+    nama: "Hafsa",
+    artinya: "pelindung"
+  }, {
+    nama: "Faisal",
+    artinya: "pengambil keputusan"
+  }, {
+    nama: "Farida",
+    artinya: "unik"
+  }, {
+    nama: "Malik",
+    artinya: "raja"
+  }];
   return [].concat(data, data2, data3);
 }
