25a973870b0f10a45e855f68210725f5884ad7af
mzaini30 <muhzaini30@gmail.com>
2023-09-28 14:37:58 +0800

    simpan dulu

---
+++ aplikasi-android/hadrah-basaudan/src/pages/halaman/[urutan].svelte
@@ -0,0 +1,13 @@
+<script>
+  export let params = {};
+
+  import data from "../../data.json";
+
+  let data_terpilih = data[params.urutan];
+
+  let kumpulan_halaman = [];
+  for (let x = data_terpilih.dari; x <= data_terpilih.ke; x++) {
+    kumpulan_halaman = [...kumpulan_halaman, x];
+  }
+  console.log(kumpulan_halaman);
+</script>
