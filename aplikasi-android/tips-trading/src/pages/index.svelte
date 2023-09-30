ae2003acf3b50532c54596d9940f9c6a2cc47dc4
mzaini30 <muhzaini30@gmail.com>
2023-09-30 16:50:47 +0800

    simpan dulu

---
+++ aplikasi-android/tips-trading/src/pages/index.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { acak } from "kumpulan-tools";
+  import data from "../tiddlers/published.json";
+  import { push } from "svelte-spa-router";
+
+  let slug_terpilih = acak(data)[0].slug;
+  push(`/baca/${slug_terpilih}`);
+</script>
--- aplikasi-android/tips-trading/tailwind.config.js
+++ aplikasi-android/tips-trading/tailwind.config.js
@@ -5,4 +5,29 @@ export default {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#d7f77e",
+
+          secondary: "#10c900",
+
+          accent: "#9dbcf2",
+
+          neutral: "#322839",
+
+          "base-100": "#f2f5f8",
+
+          info: "#6693e5",
+
+          success: "#186849",
+
+          warning: "#8e7110",
+
+          error: "#f56a66",
+        },
+      },
+    ],
+  },
 };
