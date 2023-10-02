98c8f7004ae0f5a4418f3fa2e548e58dd6f3cc66
mzaini30 <muhzaini30@gmail.com>
2023-10-02 12:03:30 +0800

    simpan

---
+++ aplikasi-android/iqro/src/pages/index.svelte
@@ -0,0 +1,10 @@
+<div class="p-4">
+  <div class=" grid grid-cols-2 gap-3">
+    {#each Array.from({ length: 6 }, (x, n) => n + 1) as z}
+      <a
+        href="#/ringkasan-iqro/{z}"
+        class="btn text-3xl btn-circle w-full h-[calc(50vw-20px)]">{z}</a
+      >
+    {/each}
+  </div>
+</div>
--- aplikasi-android/iqro/tailwind.config.js
+++ aplikasi-android/iqro/tailwind.config.js
@@ -5,4 +5,29 @@ export default {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#bcf7a3",
+
+          secondary: "#87e588",
+
+          accent: "#fcc4ce",
+
+          neutral: "#1f1424",
+
+          "base-100": "#eee7f3",
+
+          info: "#81a3ef",
+
+          success: "#2acba8",
+
+          warning: "#d58915",
+
+          error: "#ec465c",
+        },
+      },
+    ],
+  },
 };
