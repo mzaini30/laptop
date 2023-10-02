d45de016f3f685e4cff48ea4758a7ed78db18da2
mzaini30 <muhzaini30@gmail.com>
2023-10-03 05:33:07 +0800

    simpan

---
+++ aplikasi-android/matan-aqidatul-awam/src/pages/index.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Beranda from "../halaman/beranda.svx";
+</script>
+
+<div class="konten">
+  <Beranda />
+</div>
+
+<style>
+  .konten :global(*) {
+    @apply text-right text-lg;
+  }
+  .konten :global(h1),
+  .konten :global(h2) {
+    @apply text-center font-bold bg-green-500 text-white p-2;
+  }
+  .konten :global(h1) {
+    @apply mt-0;
+  }
+  .konten :global(p) {
+    @apply mx-3;
+  }
+</style>
