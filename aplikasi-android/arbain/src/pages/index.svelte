ef21c1d5daffc4fca173cd09f39f201757bc94ab
mzaini30 <muhzaini30@gmail.com>
2023-10-03 15:17:15 +0800

    simpan dulu

---
+++ aplikasi-android/arbain/src/pages/index.svelte
@@ -0,0 +1,17 @@
+<div class="p-3">
+  <div class="flex justify-between mb-3">
+    <a
+      href="https://play.google.com/store/apps/details?id=com.arbainnawawi.ibnurajab"
+      class="btn">Review App</a
+    >
+    <a
+      href="https://play.google.com/store/apps/dev?id=5401138465689796048"
+      class="btn">Other Apps</a
+    >
+  </div>
+  <ul class="menu bg-base-200 w-full rounded-box">
+    <li><a>Item 1</a></li>
+    <li><a>Item 2</a></li>
+    <li><a>Item 3</a></li>
+  </ul>
+</div>
