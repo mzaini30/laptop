fba87afb74229b253126eb9a11ed3e8bd261705e
mzaini30 <muhzaini30@gmail.com>
2023-10-04 15:10:16 +0800

    terbaru

--- aplikasi-android/catatan-penghafal-quran/src/pages/index.svelte
+++ aplikasi-android/catatan-penghafal-quran/src/pages/index.svelte
@@ -12,6 +12,16 @@
 
 <img src={gambar} alt="" class="w-20 block mx-auto" />
 <h1 class="font-[Updock] text-center text-3xl py-5">Catatan Penghafal Quran</h1>
+<div class="flex justify-between mb-4">
+  <a
+    href="https://play.google.com/store/apps/details?id=com.mzaini30.catatanpenghafalquran"
+    class="btn">Review App</a
+  >
+  <a
+    href="https://play.google.com/store/apps/dev?id=5401138465689796048"
+    class="btn">Other Apps</a
+  >
+</div>
 <ul class="menu bg-base-200 w-full rounded-box">
   {#each semua_angkatan as x}
     <li><a href="#/angkatan/{x}">Angkatan {x}</a></li>
