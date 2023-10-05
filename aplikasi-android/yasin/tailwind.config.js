c7376f1d87186002e5ca11bce6a1e1d61b780764
mzaini30 <muhzaini30@gmail.com>
2023-10-05 17:52:09 +0800

    simpan dulu

--- aplikasi-android/yasin/tailwind.config.js
+++ aplikasi-android/yasin/tailwind.config.js
@@ -5,4 +5,29 @@ export default {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#0fdcfc",
+
+          secondary: "#bae4ff",
+
+          accent: "#e07fd3",
+
+          neutral: "#15181e",
+
+          "base-100": "#f1f1f4",
+
+          info: "#7393d3",
+
+          success: "#128144",
+
+          warning: "#9b7a0d",
+
+          error: "#fb4e4b",
+        },
+      },
+    ],
+  },
 };
