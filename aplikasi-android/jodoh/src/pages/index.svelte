23698ffdb679e3bdc164dbe54318ef08804355df
mzaini30 <muhzaini30@gmail.com>
2023-10-07 07:02:59 +0800

    simpan

---
+++ aplikasi-android/jodoh/src/pages/index.svelte
@@ -0,0 +1,44 @@
+<div class="p-4">
+  <div class="flex justify-between mb-4">
+    <a
+      href="https://play.google.com/store/apps/details?id=com.mzaini30.jodoh"
+      class="btn">Review App</a
+    >
+    <a
+      href="https://play.google.com/store/apps/dev?id=5401138465689796048"
+      class="btn">Other Apps</a
+    >
+  </div>
+  <div class="w-full gap-4">
+    <div class="flex justify-center">
+      <div
+        class="radial-progress mx-auto text-pink-500"
+        style="--value:0; --size: 200px; --thickness: 2px"
+      >
+        0%
+      </div>
+    </div>
+    <div class="[&_*]:text-pink-500">
+      <div class="form-control w-full max-w-full">
+        <label class="label">
+          <span class="label-text">Namamu</span>
+        </label>
+        <input
+          type="text"
+          placeholder="Tulis di sini"
+          class="input input-bordered w-full max-w-full"
+        />
+      </div>
+      <div class="form-control w-full max-w-full">
+        <label class="label">
+          <span class="label-text">Namanya</span>
+        </label>
+        <input
+          type="text"
+          placeholder="Tulis di sini"
+          class="input input-bordered w-full max-w-full"
+        />
+      </div>
+    </div>
+  </div>
+</div>
--- aplikasi-android/jodoh/tailwind.config.js
+++ aplikasi-android/jodoh/tailwind.config.js
@@ -5,4 +5,29 @@ export default {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#6200a8",
+
+          secondary: "#8dfc9c",
+
+          accent: "#2c90d3",
+
+          neutral: "#2b1b31",
+
+          "base-100": "#f9f9fb",
+
+          info: "#6eb5e7",
+
+          success: "#38e58c",
+
+          warning: "#cc8005",
+
+          error: "#fb4652",
+        },
+      },
+    ],
+  },
 };
