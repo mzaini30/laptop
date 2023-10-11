ee69d7dfe05c3f18d7dd4ee6129d4c63bdf16604
mzaini30 <muhzaini30@gmail.com>
2023-10-11 22:47:36 +0800

    simpan

---
+++ aplikasi-android/palestine-wallpaper/src/pages/index.svelte
@@ -0,0 +1,16 @@
+<script>
+  import data from "../Download Gambar Pinterest.json";
+  import { acak } from "kumpulan-tools";
+
+  let terpilih = {};
+
+  function diacak() {
+    terpilih = acak(data)[0];
+  }
+  diacak();
+</script>
+
+<div class="h-screen overflow-hidden flex items-center">
+  <img src={terpilih.lokasi_file} alt="" />
+</div>
+<div class="w-full p-3 fixed bottom-0 left-0"></div>
--- aplikasi-android/palestine-wallpaper/tailwind.config.js
+++ aplikasi-android/palestine-wallpaper/tailwind.config.js
@@ -4,5 +4,30 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui")], daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#6200a8",
+        
+"secondary": "#8dfc9c",
+        
+"accent": "#2c90d3",
+        
+"neutral": "#2b1b31",
+        
+"base-100": "#f9f9fb",
+        
+"info": "#6eb5e7",
+        
+"success": "#38e58c",
+        
+"warning": "#cc8005",
+        
+"error": "#fb4652",
+        },
+      },
+    ],
+  },
 };
