f2a8f1209afba44f62f39a9c227e5d945a08eba1
mzaini30 <muhzaini30@gmail.com>
2023-10-19 22:45:28 +0800

    simpan

--- aplikasi-android/car-wallpaper/src/pages/index.svelte
+++ aplikasi-android/car-wallpaper/src/pages/index.svelte
@@ -28,7 +28,7 @@
     <ul class="menu bg-base-200 w-full rounded-box">
       <li>
         <a
-          href="https://play.google.com/store/apps/details?id=com.space.wallpapernew"
+          href="https://play.google.com/store/apps/details?id=com.car.wallpaper"
           >Review App</a
         >
       </li>
