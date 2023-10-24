f77b9112b38046170992dc7456859fcfd709b3c7
mzaini30 <muhzaini30@gmail.com>
2023-10-24 09:42:06 +0800

    simpan

--- aplikasi-android/sport-car-wallpaper/src/pages/index.svelte
+++ aplikasi-android/sport-car-wallpaper/src/pages/index.svelte
@@ -28,7 +28,7 @@
     <ul class="menu bg-base-200 w-full rounded-box">
       <li>
         <a
-          href="https://play.google.com/store/apps/details?id=com.cutecat.wallpapernew"
+          href="https://play.google.com/store/apps/details?id=com.sportcar.wallpaper"
           >Review App</a
         >
       </li>
