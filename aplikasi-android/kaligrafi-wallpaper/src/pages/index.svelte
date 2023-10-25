c4e110535819fdafb52507c0b97afc217add5c06
mzaini30 <muhzaini30@gmail.com>
2023-10-25 19:17:25 +0800

    andro

--- aplikasi-android/kaligrafi-wallpaper/src/pages/index.svelte
+++ aplikasi-android/kaligrafi-wallpaper/src/pages/index.svelte
@@ -28,7 +28,7 @@
     <ul class="menu bg-base-200 w-full rounded-box">
       <li>
         <a
-          href="https://play.google.com/store/apps/details?id=com.fantasy.wallpapernew"
+          href="https://play.google.com/store/apps/details?id=com.kaligrafi.wallpaper"
           >Review App</a
         >
       </li>
