c19e7bb93c8dc585e88cb84eed6059a398805fc1
mzaini30 <muhzaini30@gmail.com>
2023-10-28 19:18:08 +0800

    kasih iklan reward pada arabic sentences

--- aplikasi-android/arabic-sentences/src/pages/index.svelte
+++ aplikasi-android/arabic-sentences/src/pages/index.svelte
@@ -3,25 +3,19 @@
   import data from "../data.json";
 
   let terpilih = {};
+  let coin = 11;
 
   function ambil_terpilih() {
+    coin -= 1;
+    if (coin < 0) {
+      coin = 10;
+      Andro.reward();
+    }
     terpilih = acak(data)[0];
   }
   ambil_terpilih();
 </script>
 
-<div class="fixed left-0 top-0 p-2 w-full">
-  <div class="flex justify-between gap-2">
-    <a
-      href="https://play.google.com/store/apps/details?id=com.arabic.sentences"
-      class="btn">Review App</a
-    >
-    <a
-      href="https://play.google.com/store/apps/dev?id=5401138465689796048"
-      class="btn">Other Apps</a
-    >
-  </div>
-</div>
 <div class="hero min-h-screen bg-base-200">
   <div class="hero-content text-center">
     <div class="max-w-md">
@@ -35,7 +29,13 @@
     </div>
   </div>
 </div>
-<button
-  on:click={ambil_terpilih}
-  class="btn-primary btn-circle fixed bottom-2 right-2 text-3xl">🎲</button
->
+<div class=" fixed bottom-2 right-2">
+  <div class="indicator">
+    <div class="indicator-item badge badge-error">
+      {coin == 0 ? "Ad" : coin}
+    </div>
+    <button on:click={ambil_terpilih} class="btn-primary btn-circle text-3xl"
+      >🎲</button
+    >
+  </div>
+</div>
