c6b872eadb5b064744cadeff3f923febc1c90a8c
mzaini30 <muhzaini30@gmail.com>
2023-10-31 11:56:41 +0800

    iklan reward

--- aplikasi-android/hadrah-basaudan/src/pages/index.svelte
+++ aplikasi-android/hadrah-basaudan/src/pages/index.svelte
@@ -1,25 +1,22 @@
 <script>
   import { push } from "svelte-spa-router";
   import data from "../data.json";
+
+  let support_me = true;
 </script>
 
 <div class="p-4">
-  <button
-    class="btn mr-3"
-    on:click={() =>
-      (location.href =
-        "https://play.google.com/store/apps/details?id=com.hadrah.basaudan")}
-    >Review App</button
-  >
-  <button
-    class="btn"
-    on:click={() =>
-      (location.href =
-        "https://play.google.com/store/apps/dev?id=5401138465689796048")}
-    >Other Apps</button
-  >
-
-  <div class="join mt-3 w-full join-vertical">
+  {#if support_me}
+    <button
+      on:click={() => {
+        support_me = false;
+        Andro.reward();
+      }}
+      class="btn w-full mb-3"
+      >Support Me <div class="badge">Ad</div></button
+    >
+  {/if}
+  <div class="join w-full join-vertical">
     {#each data as x, n}
       <button
         class="btn text-lg join-item"
