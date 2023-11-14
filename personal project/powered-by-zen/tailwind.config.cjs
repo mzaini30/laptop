72028ae419e1e59e4ec0e5fc079b621d80ee0d92
mzaini30 <muhzaini30@gmail.com>
2023-11-14 13:19:41 +0800

    edit dikit

--- personal project/powered-by-zen/tailwind.config.cjs
+++ personal project/powered-by-zen/tailwind.config.cjs
@@ -6,7 +6,32 @@ const config = {
 		extend: {}
 	},
 
-	plugins: []
+	plugins: [require('@tailwindcss/typography'), require('daisyui')],
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: '#009aae',
+
+					secondary: '#007d00',
+
+					accent: '#008eff',
+
+					neutral: '#172522',
+
+					'base-100': '#f6ffeb',
+
+					info: '#00c2ff',
+
+					success: '#00a671',
+
+					warning: '#f28e00',
+
+					error: '#ff6b91'
+				}
+			}
+		]
+	}
 };
 
 module.exports = config;
