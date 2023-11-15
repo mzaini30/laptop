39be182c91ad6b9b3f2ec03e5bdc1c41a29f274e
mzaini30 <muhzaini30@gmail.com>
2023-11-15 19:41:50 +0800

    image header

--- personal project/powered-by-zen/src/bagian/header.svelte
+++ personal project/powered-by-zen/src/bagian/header.svelte
@@ -1,4 +1,8 @@
+<script>
+	import header from '../gambar/Desain_tanpa_judul__1_-removebg-preview.png';
+</script>
+
 <div class="px-[150px] py-8 bg-gray-600 text-white">
 	<div class="text-xl"><span class="font-bold">Zen</span> Hack</div>
-	<enhanced:img src="../gambar/Desain_tanpa_judul__1_-removebg-preview.png" alt="" />
+	<img src={header} alt="" />
 </div>
