d308a79923702a01a1a9a177c44a2cef1c5a9db8
mzaini30 <muhzaini30@gmail.com>
2023-11-16 08:08:08 +0800

    tambahkan copywriting sedikit

--- personal project/powered-by-zen/src/routes/+page.svelte
+++ personal project/powered-by-zen/src/routes/+page.svelte
@@ -8,5 +8,19 @@
 </svelte:head>
 
 <div class="prose max-w-full">
-	<h1>Apa Aja yang Ditawarkan oleh Zen?</h1>
+	<h2>Apa aja yang ditawarkan oleh Zen?</h2>
+	<ol>
+		<li>Perbaikan website</li>
+		<li>Perbaikan template Blogspot</li>
+		<li>Perbaikan kode HTML, CSS, dan JavaScript</li>
+		<li>Perbaikan WordPress</li>
+		<li>Pembuatan website</li>
+		<li>Pembuatan Chrome extension</li>
+		<li>Pembuatan aplikasi Android</li>
+	</ol>
+	<h2>Berapa biayanya?</h2>
+	<p>
+		Biayanya adalah 150 ribu per hari. Bisa dicicil kok. Nanti kesepakatan biaya, di awal. Sehingga,
+		bisa dipertimbangkan terlebih dahulu.
+	</p>
 </div>
