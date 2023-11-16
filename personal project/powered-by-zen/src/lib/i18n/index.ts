ef3635616f4a4f47676053e5051ae4b1e6930173
mzaini30 <muhzaini30@gmail.com>
2023-11-16 13:48:21 +0800

    terjemahan

--- personal project/powered-by-zen/src/lib/i18n/index.ts
+++ personal project/powered-by-zen/src/lib/i18n/index.ts
@@ -2,7 +2,7 @@
 import { browser } from '$app/environment'
 import { init, register } from 'svelte-i18n'
 
-const defaultLocale = 'id'
+const defaultLocale = 'en'
 
 register('en', () => import('./locales/en.json'))
 register('id', () => import('./locales/id.json'))
