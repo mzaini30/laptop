7eca846c849f42147fac80c55378da8588573ee5
mzaini30 <muhzaini30@gmail.com>
2023-11-16 08:01:47 +0800

    init inlang

--- personal project/powered-by-zen/src/routes/+page.svelte
+++ personal project/powered-by-zen/src/routes/+page.svelte
@@ -1,7 +1,12 @@
 <script>
 	import Header from '../bagian/header.svelte';
+	import * as m from '../paraglide/messages';
 </script>
 
 <svelte:head>
 	<title>Zen Hack</title>
 </svelte:head>
+
+<div class="prose max-w-full">
+	<h1>Apa Aja yang Ditawarkan oleh Zen?</h1>
+</div>
