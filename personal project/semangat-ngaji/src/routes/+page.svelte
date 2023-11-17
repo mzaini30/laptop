61065441dda4280b02c40f9a02a28a92c457cad3
mzaini30 <muhzaini30@gmail.com>
2023-11-17 10:35:56 +0800

    simpan aja

--- personal project/semangat-ngaji/src/routes/+page.svelte
+++ personal project/semangat-ngaji/src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<script lang="ts">
+	import { _ as t } from 'svelte-i18n';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
