816591faf72d9ee7ca96392a2c96339626a17085
mzaini30 <muhzaini30@gmail.com>
2023-11-21 17:08:02 +0800

    simpan aja lah

--- personal project/powered-by-zen/lawas/src/bagian/header.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import header from '../gambar/Desain_tanpa_judul__1_-removebg-preview.png';
-</script>
-
-<div class="px-[150px] py-8 bg-gray-600 text-white">
-	<div class="text-xl"><span class="font-bold">Zen</span> Hack</div>
-	<img src={header} alt="" />
-</div>
