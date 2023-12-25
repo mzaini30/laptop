8f1007c1063bbf83d9a4532b6872941670e1f7e8
mzaini30 <muhzaini30@gmail.com>
2023-12-26 07:23:04 +0800

    upload

--- kerjaan/mabaiz-laravel/app/Filament/Resources/StudyNoteResource.php
+++ kerjaan/mabaiz-laravel/app/Filament/Resources/StudyNoteResource.php
@@ -6,9 +6,15 @@ use App\Filament\Resources\StudyNoteResource\Pages;
 use App\Filament\Resources\StudyNoteResource\RelationManagers;
 use App\Models\StudyNote;
 use Filament\Forms;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -23,8 +29,21 @@ class StudyNoteResource extends Resource
     {
         return $form
             ->schema([
+                TextInput::make("judul")->required(),
+                Grid::make([
+                    "default" => "2"
+                ])->schema([
+                            TextInput::make("tujuan")->required(),
+                            DatePicker::make("tanggal")->required(),
+                        ]),
                 //
-            ]);
+
+
+                RichEditor::make("catatan_perjalanan")->required()
+                //     $table->date("tanggal");
+                // $table->string("tujuan");
+                // $table->text("catatan_perjalanan");
+            ])->columns(1);
     }
 
     public static function table(Table $table): Table
@@ -32,6 +51,13 @@ class StudyNoteResource extends Resource
         return $table
             ->columns([
                 //
+                // TextInput::make("judul")->required(),
+                // DatePicker::make("tanggal")->required(),
+                // TextInput::make("tujuan")->required(),
+                // RichEditor::make("catatan_perjalanan")->required()
+                TextColumn::make("judul")->searchable()->sortable(),
+                TextColumn::make("tanggal"),
+                TextColumn::make("tujuan")->sortable()->searchable()
             ])
             ->filters([
                 //
