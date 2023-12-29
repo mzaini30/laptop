f4b5152b414276abeb761a4921c6ec9b6f055f0d
mzaini30 <muhzaini30@gmail.com>
2023-12-30 06:27:28 +0800

    upload

---
+++ npm/login-zen/src/komponen/login.svelte
@@ -0,0 +1,30 @@
+<script>
+  let sudah_login = true;
+
+  if (!localStorage.email) {
+    sudah_login = false;
+  }
+</script>
+
+{#if !sudah_login}
+  <div class="w-full h-full fixed top-0 p-5 left-0 bg-gray-200">
+    <div class="card bg-base-100 shadow-xl w-full">
+      <div class="card-body">
+        <form action="">
+          <label class="form-control w-full mb-3">
+            <div class="label">
+              <span class="label-text">Login with email</span>
+            </div>
+            <input
+              type="email"
+              required
+              placeholder="Type here"
+              class="input input-bordered w-full"
+            />
+          </label>
+          <button class="btn btn-secondary">Login</button>
+        </form>
+      </div>
+    </div>
+  </div>
+{/if}
